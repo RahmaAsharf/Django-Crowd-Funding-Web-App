13995f21c21b44a6d9695be22c659013b2be1e4d
shrookmuhamed <shrouk.muhamedd@gmail.com>
2024-03-22 16:49:12 +0200

    fixed manytomany tags relation . added base.html , create_project.html style.

--- projects/forms.py
+++ projects/forms.py
@@ -1,11 +1,36 @@
 from django import forms
 from projects.models import Project, Image
+from .models import Tag, Category
+from django.core.exceptions import ValidationError
 
+# class ProjectForm(forms.ModelForm):
+#     class Meta:
+#         model= Project
+#         fields='__all__'
 class ProjectForm(forms.ModelForm):
-    class Meta:
-        model= Project
-        fields='__all__'
+    # Add category field
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), required=True, label='Category')
+    # Add tags field
+    tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all(), required=False, label='Tags')
 
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+    # def clean(self):
+    #     cleaned_data = super().clean()
+    #     start_date = cleaned_data.get('startDate')
+    #     end_date = cleaned_data.get('endDate')
+    #     total = cleaned_data.get('total')
+    #     print("Total:", total)
+    #     if total and total <= 0:
+    #         self.add_error('total', 'Total must be greater than zero.')
+
+
+    #     if start_date and end_date and start_date >= end_date:
+    #         self.add_error('endDate', 'End date must be greater than start date.')
+
+    #     return cleaned_data
 
 # class ImageForm(forms.ModelForm):
 #     class Meta:
--- projects/views.py
+++ projects/views.py
@@ -4,25 +4,78 @@ from projects.models import Project, Image, Tag, Category
 from projects.forms import ProjectForm, ImageForm
 
 def create_project(request):
-
     if request.method == "POST":
         form = ProjectForm(request.POST)
         files = request.FILES.getlist("image")
         if form.is_valid():
-            f = form.save(commit=False)
-            f.user = request.user
-            f.save()
-            for i in files:
-                Image.objects.create(project=f, image=i)
-            return HttpResponse("added")#-----------
-        else:
-            print(form.errors)
+            project = form.save(commit=False)
+            project.user = request.user
+            project.save()
+            for file in files:
+                Image.objects.create(project=project, image=file)
+            form.save_m2m()  # Save many-to-many relationships
+            return HttpResponse("Project added successfully")
     else:
         form = ProjectForm()
-        imageform = ImageForm()
+        image_form = ImageForm()
+    
+    tags = Tag.objects.all()
+    categories = Category.objects.all()
 
-    return render(request, "projects/create_project.html", {"form": form, "imageform": imageform})
+    return render(request, "projects/create_project.html", {"form": form, "imageform": image_form, "tags": tags, "categories": categories})
+
+# def create_project(request):
+
+#     if request.method == "POST":
+#         form = ProjectForm(request.POST)
+#         files = request.FILES.getlist("image")
+#         if form.is_valid():
+#             f = form.save(commit=False)
+#             f.user = request.user
+#             f.save()
+#             for i in files:
+#                 Image.objects.create(project=f, image=i)
+#             return HttpResponse("added")#-----------
+#         else:
+#             print(form.errors)
+#     else:
+#         form = ProjectForm()
+#         imageform = ImageForm()
+
+#     return render(request, "projects/create_project.html", {"form": form, "imageform": imageform})
 
 
 # def try1(request):
 #     return HttpResponse('hello')
+
+# def create_project(request):
+#     if request.method == "POST":
+#         form = ProjectForm(request.POST)
+#         files = request.FILES.getlist("image")
+#         if form.is_valid():
+#             f = form.save(commit=False)
+#             f.user = request.user
+#             f.save()
+            
+#             # Process and save tags
+#             tag_names = request.POST.getlist("tags")  # Assuming the tags are submitted as a list
+#             for tag_name in tag_names:
+#                 tag, created = Tag.objects.get_or_create(name=tag_name)
+#                 f.tags.add(tag)
+            
+#             # Save images
+#             for i in files:
+#                 Image.objects.create(project=f, image=i)
+            
+#             return HttpResponse("added")
+#         else:
+#             print(form.errors)
+#     else:
+#         form = ProjectForm()
+#         imageform = ImageForm()
+#         tags = Tag.objects.all()
+#         return render(request, "projects/create_project.html", {"form": form, "imageform": imageform, "tags": tags})
+
+#     tags = Tag.objects.all()
+#     return render(request, "projects/create_project.html", {"form": form, "imageform": imageform, "tags": tags})
+
