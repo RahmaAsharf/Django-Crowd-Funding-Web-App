addf78289ffef97b842aa1626bc5e0f7ecf86154
Rahma Hassan <rahmaashraf053@gmail.com>
2024-03-28 01:01:08 +0200

    refactoring

--- authentication/models.py
+++ authentication/models.py
@@ -18,7 +18,7 @@ class CustomUser(AbstractUser):
   first_name = models.CharField(max_length=100)
   last_name = models.CharField(max_length=100)
   mobile_phone = models.CharField(max_length=11)
-  profile_picture = models.ImageField(upload_to='media', null=True) # (REQUIRED)
+  profile_picture = models.ImageField(upload_to='auth', null=True) # (REQUIRED)
   confirm_password = models.CharField(max_length=100)
   activation_key = models.UUIDField(default=uuid.uuid4, editable=False)
   birthdate = models.DateField(null=True, blank=True)
